54a0d5657a260a7756bf8c0e262c72a6a82c84f4
tyri0n98 <michal@arch.localdomain>
2026-02-15 16:13:28 +0100

    fix after hyprland update

--- waypaper/set_wallpaper.sh
+++ waypaper/set_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hyprctl hyprpaper reload ,$1
+hyprctl hyprpaper wallpaper ,$1,
 rm ~/.config/hypr/hyprpaper.conf
 echo "preload = $1" > ~/.config/hypr/hyprpaper.conf
 echo "wallpaper = , $1" >> ~/.config/hypr/hyprpaper.conf
